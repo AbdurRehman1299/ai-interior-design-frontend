9500a06d5ff19d1f2c08e3571d9c6d561ae9f71c
Abdur Rehman <abdurrehman76001@gmail.com>
2025-11-17 20:39:24 +0500

    Add new furniture

--- src/app/data/furnitureData.js
+++ src/app/data/furnitureData.js
@@ -1,20 +1,20 @@
 export const furniture = [
     {
       id: 'modern-sofa',
-      name: 'Modern Sofa',
+      name: 'Sofa',
       file: '/models/sofa.glb',
       tags: ['living room', 'sofa', 'black'],
     },
     {
-      id: 'wooden-table',
-      name: 'Wooden Table',
-      file: '/models/sofa (1).glb',
+      id: 'industrial-table',
+      name: 'Table',
+      file: '/models/industrial_table.glb',
       tags: ['brown', 'wood', 'classic room'],
     },
     {
-      id: 'floor-lamp',
-      name: 'Floor Lamp',
-      file: '/models/sofa_glb.glb',
+      id: 'bar-stool',
+      name: 'Stool',
+      file: '/models/bar_stool.glb',
       tags: ['red', 'metal', 'modern room'],
     },
 ];
